3c19b37df8b1b15b49b8dda28be5aa412175e86f
Ryan <ryan.maceachern@gmail.com>
2022-08-17 22:57:37 -0700

    add time to timer card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
 import './App.css'
+import Timer from './components/Timer'
+
 
 function App() {
   return (
     <div>
-      <p>Clean React App</p>
+      <p>PomoScapes</p>
+      <Timer />
     </div>
   )
 }
---
+++ src/components/Timer.jsx
@@ -0,0 +1,13 @@
+import Paper from "@mui/material/Paper";
+
+const Timer = () => {
+  return (
+    <div>
+      <Paper elevation={12}>
+        <h1>00:00</h1>
+      </Paper>
+    </div>
+  );
+}
+
+export default Timer;
