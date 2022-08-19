9a6586c01e7b9bae04ae966f060ffcfc5597b0c4
Ryan <ryan.maceachern@gmail.com>
2022-08-18 21:35:54 -0700

    add Context hook for timer, wrapping App components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React, {useState} from 'react';
 import './App.css'
 import Timer from './components/Timer'
 
+export const Context = React.createContext();
+
 
 function App() {
+  const [timer, setTimer] = useState(25);
+  
   return (
-    <div>
-      <p>PomoScapes</p>
-      <Timer />
-    </div>
+    <Context.Provider value={[timer, setTimer]}>
+      <div>
+        <p>PomoScapes</p>
+        <Timer />
+      </div>
+    </Context.Provider>
   )
 }
 
