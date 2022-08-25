cb79243e876edeb1fdf645f0ae8cdfb764fe518d
Rmaceac <ryan.maceachern@gmail.com>
2022-08-25 16:14:44 -0700

    import and implement reactjs-countdown-hook package

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,22 +1,46 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+import { useTimer } from 'reactjs-countdown-hook';
 import Paper from "@mui/material/Paper";
 
 const Timer = () => {
-  const [minutes, setMinutes] = useState(25);
-  const [seconds, setSeconds] = useState(0);
+  // const [minutes, setMinutes] = useState(25);
+  // const [seconds, setSeconds] = useState(0);
   // const [showMessage, setShowMessage] = (false);
 
-const displayMinutes = minutes < 10 ? `0${minutes}` : minutes;
-const displaySeconds = seconds < 10 ? `0${seconds}` : seconds;
+// useEffect(() => {
+  // 
+// }, [seconds]);
 
-useEffect(() => {
+const handleTimerFinish = () => {
+  alert("Time is up!");
+}
 
-}, []);
+const { 
+  isActive,
+  counter,
+  seconds,
+  minutes,
+  hours,
+  pause,
+  resume,
+  reset
+} = useTimer (120, handleTimerFinish);
+
+// const displayMinutes = minutes < 10 ? `0${minutes}` : minutes;
+// const displaySeconds = seconds < 10 ? `0${seconds}` : seconds;
 
   return (
     <div>
       <Paper elevation={12}>
-        <h1>{displayMinutes}:{displaySeconds}</h1>
+        <div>
+          <span>{minutes}</span>:<span>{seconds}</span>
+        </div>
+        <div>
+          <button onClick={() => (isActive ? pause() : resume())}>
+            {isActive ? 'Pause' : 'Resume'}
+          </button>
+          <button onClick={reset}>Reset</button>
+        </div>
       </Paper>
     </div>
   );
