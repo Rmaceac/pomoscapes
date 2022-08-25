5f605b4450db5d9d7a4a8962c6315f4c014e2fd4
Rmaceac <ryan.maceachern@gmail.com>
2022-08-25 15:24:06 -0700

    create state for timer minutes and seconds

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,10 +1,18 @@
+import { useState, useEffect } from 'react';
 import Paper from "@mui/material/Paper";
 
 const Timer = () => {
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
+  // const [showMessage, setShowMessage] = (false);
+
+
+
+
   return (
     <div>
       <Paper elevation={12}>
-        <h1>00:00</h1>
+        <h1>{minutes}:{seconds}</h1>
       </Paper>
     </div>
   );
