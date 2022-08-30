a22ec90d0d7954afe6f6b5b98fdd40e85eb95276
Ryan <ryan.maceachern@gmail.com>
2022-08-30 13:50:28 -0700

    remove useContext wrapper from App.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,13 @@ import React, {useState} from 'react';
 import './App.css'
 import Timer from './components/Timer'
 
-export const Context = React.createContext();
-
-
 function App() {
-  const [timer, setTimer] = useState({
-    pomodoro: 25,
-    shortBreak: 5,
-    longBreak: 15,
-    longBreakInterval: 4
-  });
   
   return (
-    <Context.Provider value={[timer, setTimer]}>
       <div>
         <p>PomoScapes</p>
         <Timer />
       </div>
-    </Context.Provider>
   )
 }
 
