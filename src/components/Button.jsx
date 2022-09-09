c571ef265272a4d83cb1573e7432146548278608
Rmaceac <ryan.maceachern@gmail.com>
2022-09-09 14:23:35 -0700

    create Button component, setup to take props incl. style and size props

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import { Button } from '@mui/material'
+
+const STYLES = ['btn--primary', 'btn--outline', 'btn--secondary'];
+const SIZES = ['btn--medium', 'btn--small', 'btn--large'];
+
+const Button = ({
+  label,
+  buttonStyle,
+  buttonSize,
+  onClick
+}) => {
+  
+  return (
+    <Button></Button>
+  )
+}
+
+export default Button
